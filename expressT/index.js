995e848ac99c7be81840d82494e08b89febad640
DubemN12 <122755811+DubemN12@users.noreply.github.com>
2023-03-30 23:37:45 -0500

    End

--- expressT/index.js
+++ expressT/index.js
@@ -2,26 +2,34 @@ const express = require('express') // Requires the express module
 
 const app = express()
 
-app.listen(3000,() =>{
-    console.log('App listening to port 3000')
-})
-
 
 // The allows us to make API's
 // This is also how routing works
 app.get('/',(req, res) =>{
-    res.json({name: 'Dubem Nwachukwu'})
+    //res.json({name: 'Dubem Nwachukwu'})
     res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/about', (req,res) =>{
     res.json({
-        name: 'The End'
+        name: 'The End'     
     })
+    res.sendFile(__dirname + '/about.html')
 })
 
 app.get('/contact', (req, res) =>{
     res.json(
         {name: 'Ledo Green'}
     )
+    res.sendFile(__dirname + 'contact.html')
+})
+
+app.get('/bgd', (res,req) =>{
+    res.use("Check Color")
+})
+
+app.use(express.static('public'))
+
+app.listen(3000,() =>{
+    console.log('App listening to port 3000')
 })
